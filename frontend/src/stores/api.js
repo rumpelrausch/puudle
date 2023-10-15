a9ad1ef2dac569d27e044fef1eb1726e69f2af8a
flp <flp@schicknemail.de>
2023-10-15 12:12:32 +0200

    feat: api url depends upon environment

--- frontend/src/stores/api.js
+++ frontend/src/stores/api.js
@@ -1,22 +1,11 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const URL_API = 'http://localhost:8080/api/v0';
+const URL_API = process.env.DEV ? 'http://localhost:8080/api/v0' : '/api/v0';
 
 export const apiStore = defineStore('counter', {
   state: () => ({
     entries: []
-    //   {
-    //     _id: '1234',
-    //     name: 'Bouldern / FLASHH',
-    //     date: '2023-10-17 17:00'
-    //   },
-    //   {
-    //     _id: '1234',
-    //     name: 'Bouldern / FLASHH',
-    //     date: '2023-10-16 11:00'
-    //   }
-    // ]
   }),
 
   getters: {
