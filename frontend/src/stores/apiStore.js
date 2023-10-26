fd2f98e8074e8ee7a87f2dcad2c2f33219f546db
flp <flp@schicknemail.de>
2023-10-26 18:22:59 +0200

    refactor: removes intermediate store

--- frontend/src/stores/apiStore.js
+++ frontend/src/stores/apiStore.js
@@ -61,141 +61,140 @@ function getLocalSettings() {
   return localSettings;
 }
 
-export const apiStore = () => {
-  const innerStore = defineStore(STORE_ID, () => {
-    const env = useQuasar().config.customEnv;
-    POLL_INTERVAL_MS = env.POLL_INTERVAL_MS || 2000;
-    DELAYED_POLL_MS = env.DELAYED_POLL_MS || 15000;
-    GET_ONLY_CURRENT_ENTRIES = !!env.GET_ONLY_CURRENT_ENTRIES;
+export const apiStore = defineStore(STORE_ID, () => {
+  const env = useQuasar().config.customEnv;
+  POLL_INTERVAL_MS = env.POLL_INTERVAL_MS || 2000;
+  DELAYED_POLL_MS = env.DELAYED_POLL_MS || 15000;
+  GET_ONLY_CURRENT_ENTRIES = !!env.GET_ONLY_CURRENT_ENTRIES;
 
-    const entries = ref([]);
-    const entriesStamp = ref([0]);
-    const currentErrorMessage = ref('');
-    const userSettings = ref(getLocalSettings());
+  const entries = ref([]);
+  const entriesStamp = ref([0]);
+  const currentErrorMessage = ref('');
+  const userSettings = ref(getLocalSettings());
 
-    watch(userSettings, () => {
-      localStorage.setItem(STORE_ID, JSON.stringify(userSettings.value));
-    }, { deep: true });
+  function updateStamp() {
+    entriesStamp.value[0] = new Date().getTime();
+  }
 
-    return {
-      entries,
-      entriesStamp,
-      currentErrorMessage,
-      userSettings,
+  async function fetchEntries(force = false) {
+    const myEntries = await poll(force);
+    if (!myEntries) {
+      return;
+    }
+    entries.value = enrichEntries(myEntries);
+    updateStamp();
+  }
 
-      getHijackFilter() {
-        return !duringPoll;
-      },
-
-      startUpdate() {
-        if (updateTimer) {
-          return;
-        }
-        updateTimer = setInterval(this.fetchEntries, POLL_INTERVAL_MS);
-      },
-
-      suspendUpdate() {
-        isPollAllowed = false;
-        if (pollDelayTimer) {
-          clearTimeout(pollDelayTimer);
-        }
-        pollDelayTimer = setTimeout(() => { isPollAllowed = true; }, DELAYED_POLL_MS);
-      },
-
-      resumeUpdate() {
-        if (pollDelayTimer) {
-          clearTimeout(pollDelayTimer);
-        }
-        isPollAllowed = true;
-      },
-
-      updateStamp() {
-        this.entriesStamp[0] = new Date().getTime();
-      },
-
-      async fetchEntries(force = false) {
-        const entries = await poll(force);
-        if (!entries) {
-          return;
-        }
-        this.entries = enrichEntries(entries);
-        this.updateStamp();
-      },
-
-      getEntryById(entryId) {
-        return this.entries.find((entry) => entry._id === entryId);
-      },
-
-      async addEntry(entryName, realDate) {
-        const body = {
-          type: 'entry',
-          subscriptions: [],
-          entryName,
-          date: convertRealDateToDB(realDate)
-        };
-        const response = await axios.post(`${URL_API}/entry`, body);
-
-        if (response.status === 409) {
-          throw new Error('errorMessages.entryAlreadyExists');
-        }
-
-        await this.fetchEntries(true);
-      },
-
-      async deleteEntry(entryId) {
-        const response = await axios.delete(`${URL_API}/entry/${entryId}`);
-
-        if (response.status !== 200) {
-          throw new Error('errorMessages.genericApiError');
-        }
-
-        await this.fetchEntries(true);
-      },
-
-      async addSubscription(entryId, subscription) {
-        const response = await axios.post(`${URL_API}/entry/${entryId}/subscription`, subscription);
-
-        if (response.status === 409) {
-          throw new Error('errorMessages.entryAlreadyExists');
-        }
-
-        await this.fetchEntries(true);
-        return this.getEntryById(entryId);
-      },
-
-      async updateSubscription(entryId, userName, subscription) {
-        const response = await axios.patch(`${URL_API}/entry/${entryId}/subscription/${userName}`, {
-          state: subscription.state,
-          comment: subscription.comment
-        });
-        if (!response) {
-          return null;
-        }
-        await this.fetchEntries(true);
-      },
-
-      async deleteSubscription(entryId, userName) {
-        const response = await axios.delete(`${URL_API}/entry/${entryId}/subscription/${userName}`);
-        if (!response) {
-          return null;
-        }
-        await this.fetchEntries(true);
-        return this.getEntryById(entryId);
-      }
-    };
-  });
-
-  const store = innerStore();
   axios.interceptors.response.use(function (response) {
     if (response) {
-      store.$patch({ currentErrorMessage: '' });
+      currentErrorMessage.value = '';
     }
     return response;
   }, (error) => {
-    store.$patch({ currentErrorMessage: error.message });
+    currentErrorMessage.value = error.message;
     return error.response;
   });
 
-  store.fetchEntries(true);
-  return store;
-};
+  watch(userSettings, () => {
+    localStorage.setItem(STORE_ID, JSON.stringify(userSettings.value));
+  }, { deep: true });
+
+  fetchEntries(true);
+
+  return {
+    entries,
+    entriesStamp,
+    currentErrorMessage,
+    userSettings,
+
+    updateStamp,
+    fetchEntries,
+
+    getHijackFilter() {
+      return !duringPoll;
+    },
+
+    startUpdate() {
+      if (updateTimer) {
+        return;
+      }
+      updateTimer = setInterval(this.fetchEntries, POLL_INTERVAL_MS);
+    },
+
+    suspendUpdate() {
+      isPollAllowed = false;
+      if (pollDelayTimer) {
+        clearTimeout(pollDelayTimer);
+      }
+      pollDelayTimer = setTimeout(() => { isPollAllowed = true; }, DELAYED_POLL_MS);
+    },
+
+    resumeUpdate() {
+      if (pollDelayTimer) {
+        clearTimeout(pollDelayTimer);
+      }
+      isPollAllowed = true;
+    },
+
+    getEntryById(entryId) {
+      return this.entries.find((entry) => entry._id === entryId);
+    },
+
+    async addEntry(entryName, realDate) {
+      const body = {
+        type: 'entry',
+        subscriptions: [],
+        entryName,
+        date: convertRealDateToDB(realDate)
+      };
+      const response = await axios.post(`${URL_API}/entry`, body);
+
+      if (response.status === 409) {
+        throw new Error('errorMessages.entryAlreadyExists');
+      }
+
+      await this.fetchEntries(true);
+    },
+
+    async deleteEntry(entryId) {
+      const response = await axios.delete(`${URL_API}/entry/${entryId}`);
+
+      if (response.status !== 200) {
+        throw new Error('errorMessages.genericApiError');
+      }
+
+      await this.fetchEntries(true);
+    },
+
+    async addSubscription(entryId, subscription) {
+      const response = await axios.post(`${URL_API}/entry/${entryId}/subscription`, subscription);
+
+      if (response.status === 409) {
+        throw new Error('errorMessages.entryAlreadyExists');
+      }
+
+      await this.fetchEntries(true);
+      return this.getEntryById(entryId);
+    },
+
+    async updateSubscription(entryId, userName, subscription) {
+      const response = await axios.patch(`${URL_API}/entry/${entryId}/subscription/${userName}`, {
+        state: subscription.state,
+        comment: subscription.comment
+      });
+      if (!response) {
+        return null;
+      }
+      await this.fetchEntries(true);
+    },
+
+    async deleteSubscription(entryId, userName) {
+      const response = await axios.delete(`${URL_API}/entry/${entryId}/subscription/${userName}`);
+      if (!response) {
+        return null;
+      }
+      await this.fetchEntries(true);
+      return this.getEntryById(entryId);
+    }
+  };
+});
